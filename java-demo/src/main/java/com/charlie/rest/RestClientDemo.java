3a717c5658eaa682c75e92ffe760a17e161ea920
Charlie <spadem45420@gmail.com>
2022-09-05 21:55:27 +0800

    first commit

---
+++ java-demo/src/main/java/com/charlie/rest/RestClientDemo.java
@@ -0,0 +1,26 @@
+package com.charlie.rest;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class RestClientDemo {
+
+	public static void main(String[] args) {
+		Client client = ClientBuilder.newClient();
+		// demo api
+		String link = "https://script.google.com/macros/s/AKfycbyPthJ8iO6B1fPR1YBVvltBQz4dvAnrtE48sw0VxqMIW-XwejTsS4db3Pgw3oUWA4U/exec";
+		Response response = client
+				.target(link)
+				.request(MediaType.APPLICATION_JSON)
+				.get();
+		
+		// read response body
+		String body = response.readEntity(String.class);
+		
+		System.out.println("response = " + response);
+		System.out.println("response.getStatus() = " + response.getStatus());
+		System.out.println("body = " + body);
+    }
+}
