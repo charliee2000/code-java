588ed57fc11daffd9740dda883a1519997391ba2
Charlie <spadem45420@gmail.com>
2022-09-05 22:27:30 +0800

    Rest demo add junit

--- java-demo/src/main/java/com/charlie/rest/RestClientDemo.java
+++ java-demo/src/main/java/com/charlie/rest/RestClientDemo.java
@@ -5,6 +5,8 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.junit.Assert;
+
 public class RestClientDemo {
 
 	public static void main(String[] args) {
@@ -22,5 +24,8 @@ public class RestClientDemo {
 		System.out.println("response = " + response);
 		System.out.println("response.getStatus() = " + response.getStatus());
 		System.out.println("body = " + body);
+		
+		// junit check
+		Assert.assertEquals(200, response.getStatus());
     }
 }
